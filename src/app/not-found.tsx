269962192fa2df89b0f2ba05fd014e7018b7ed23
Arturo Muñoz <101126374+munozarturo@users.noreply.github.com>
2023-10-23 19:47:39 -0400

    added 404 not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import type { Metadata } from "next";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import * as React from "react";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "404 Not Found",
+  description: "Content not found.",
+};
+
+export default function Send() {
+  return (
+    <main className="w-full h-full flex flex-row justify-center items-center">
+      <Card className="w-3/5 h-3/4 flex flex-col items-center justify-center">
+        <CardHeader className="h-fit w-full">
+          <CardTitle as="h1" className="text-primary text-center">
+            404, not found
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <h2>uh oh! it looks like you're lost...</h2>
+        </CardContent>
+        <CardFooter>
+          <Link
+            href="/."
+            className="group transition duration-300 text-center text-white text-lg font-bold italic bg-primary p-2 rounded-xl"
+          >
+            take me home
+          </Link>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
--- src/app/send/page.tsx
+++ src/app/send/page.tsx
@@ -1,14 +1,67 @@
-import type { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: "Send a transfer",
-  description: "Send a transfer.",
-};
+import type { Metadata } from "next";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import * as React from "react";
+import { useState } from "react";
 
 export default function Send() {
+  const [file, setFile] = useState<File>();
+  const [title, setTitle] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {};
+
   return (
-    <main className="w-full h-full ">
-      <h1>Send</h1>
+    <main className="w-full h-full flex flex-row justify-center items-center">
+      <Card className="w-3/5 h-3/4 flex flex-row">
+        <form onSubmit={onSubmit} className="w-full h-full flex flex-row">
+          <div className="w-1/2 h-full flex flex-col">
+            <CardHeader className="h-fit w-full">
+              <CardTitle as="h1" className="text-primary text-center">
+                send
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="h-full w-full flex flex-col space-y-2">
+              <div className="flex flex-col">
+                <input
+                  className="text-primary text-xl font-semibold outline-none"
+                  type="text"
+                  name="title"
+                  placeholder="your title"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <textarea
+                className="h-full text-primary outline-none"
+                name="message"
+                placeholder="your message"
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+              <input
+                type="submit"
+                value="get a link"
+                className="h-fit w-full bg-primary rounded-xl p-2 text-secondary italic font-extrabold text-xl"
+              />
+            </CardContent>
+          </div>
+          <div className="w-1/2 h-full bg-red-500">
+            <CardContent>
+              <input
+                type="file"
+                name="file"
+                onChange={(e) => setFile(e.target.files?.[0])}
+              />
+            </CardContent>
+          </div>
+        </form>
+      </Card>
     </main>
   );
 }
