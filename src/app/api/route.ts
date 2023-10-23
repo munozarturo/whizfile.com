d9b03545d8340b50b58a24f5fe9be49814478f95
Arturo Muñoz <101126374+munozarturo@users.noreply.github.com>
2023-10-22 22:54:11 -0400

    added logging rateLimiter

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,11 +1,13 @@
 import { connectToDatabase } from "@/db/mongo";
+import { applyRateLimit } from "@/lib/api/rate-limiter";
+import { NextRequest } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
+  applyRateLimit(req);
+
   const client = await connectToDatabase();
   const db = client.db("main");
   const transfers = db.collection("transfers");
 
-  const resp = await transfers.find({}).toArray();
-
-  return Response.json({ message: "Hello world", data: resp }, { status: 200 });
+  return Response.json({ message: "Hello world" }, { status: 200 });
 }
---
+++ src/lib/api/rate-limiter.ts
@@ -0,0 +1,16 @@
+import { connectToDatabase } from "@/db/mongo";
+import { NextRequest } from "next/server";
+
+export async function applyRateLimit(req: NextRequest) {
+  const client = await connectToDatabase();
+  const db = client.db("main");
+  const requests = db.collection("requests");
+
+  const reqData = {
+    ip: req.ip || null,
+    method: req.method,
+    url: req.url,
+  };
+
+  await requests.insertOne(reqData);
+}
