d2b5a1997a1013ffae905c5a4a9c2f0ae9caedf3
Arturo Muñoz <101126374+munozarturo@users.noreply.github.com>
2023-10-24 10:15:35 -0400

    added very basic receive/transferId

--- src/app/receive/[transferId]/page.tsx
+++ src/app/receive/[transferId]/page.tsx
@@ -1,16 +1,47 @@
-import type { Metadata } from "next";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Suspense } from "react";
 
-export const metadata: Metadata = {
-  title: "Receive a transfer",
-  description: "Receive a transfer.",
-};
+if (!process.env.BASE_URL) {
+  throw new Error("BASE_URL environment variable not defined.");
+}
 
-export default function ReceiveTransferId(context: {
+const baseUrl = process.env.BASE_URL;
+
+async function Transfer({ transferId }: { transferId: string }) {
+  // Wait for the playlists
+  const res = await fetch(`${baseUrl}/api/transfer/${transferId}`);
+  const transfer = await res.json();
+
+  return (
+    <div>
+      <h1>{transfer.title}</h1>
+      <p>{transfer.message}</p>
+      <p>{transfer.createdAt}</p>
+    </div>
+  );
+}
+
+export default async function ReceiveTransferId(context: {
   params: { transferId: string };
 }) {
   return (
-    <main className="w-full h-full ">
-      <h1>Receive {context.params.transferId}</h1>
+    <main className="w-full h-full flex flex-row items-center justify-center">
+      <Card className="w-3/5 h-3/4 flex flex-col">
+        <CardHeader className="h-fit w-full">
+          <CardTitle as="h1" className="text-primary text-4xl text-center">
+            receive a transfer
+          </CardTitle>
+        </CardHeader>
+        <Suspense fallback={"loading..."}>
+          <Transfer transferId={context.params.transferId} />
+        </Suspense>
+      </Card>
     </main>
   );
 }
