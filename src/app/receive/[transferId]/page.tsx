23966f4814e16daa7dddbb1b8dc67985c84e0034
Arturo Muñoz <101126374+munozarturo@users.noreply.github.com>
2023-11-23 13:24:17 -0500

    added loading state and simple transfer view

--- src/app/receive/[transferId]/page.tsx
+++ src/app/receive/[transferId]/page.tsx
@@ -1,76 +1,116 @@
 "use client";
 
 import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-  CardFooter,
+    Card,
+    CardHeader,
+    CardTitle,
+    CardContent,
+    CardFooter,
 } from "@/components/ui/card";
 import Transfer from "@/db/models/transfer";
 import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "@/lib/api/axios-instance";
+import { PulseLoader } from "react-spinners";
 
 if (!process.env.NEXT_PUBLIC_BASE_URL) {
-  throw new Error("BASE_URL environment variable not defined.");
+    throw new Error("BASE_URL environment variable not defined.");
 }
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
 function TransferView({ transferId }: { transferId: string }) {
-  const { data, error, refetch, isError, isLoading } = useQuery({
-    queryKey: ["transferQuery", transferId],
-    queryFn: async () => {
-      const res = await axiosInstance.get(`/api/transfer/${transferId}`)
-      return res.data;
+    const { data, error, refetch, isError, isLoading } = useQuery({
+        queryKey: ["transferQuery", transferId],
+        queryFn: async () => {
+            const res = await axiosInstance.get(`/api/transfer/${transferId}`);
+            return res.data;
+        },
+    });
+
+    let transfer: Transfer | undefined;
+
+    if (data) {
+        transfer = data.data as Transfer;
     }
-  });
 
-  const downloadTransfer = async (data: Transfer) => {
-    try {
-      console.log(`${process.env.NEXT_PUBLIC_BASE_URL}/api/file/${data.fileKey}`);
-      const response = await axiosInstance.get(`/api/file/${data.fileKey}`, { responseType: 'blob' });
-      const url = window.URL.createObjectURL(response.data);
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', `whizfile-transfer-${data.transferId}.zip`); // Replace 'file.ext' with the filename
-      document.body.appendChild(link);
-      link.click();
-      
-      if (link.parentNode) {
-        link.parentNode.removeChild(link);
-      }
+    const downloadTransfer = async (transfer: Transfer | undefined) => {
+        if (!transfer) throw new Error("Transfer is undefined.");
 
-    } catch (error) {
-      console.error('Error downloading the file', error);
-    }
-  };
+        try {
+            const transferId: string = transfer.transferId;
+            const fileKey: string | null = transfer.fileKey;
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>An error has occurred: {error.message}</div>;
+            console.log(
+                `${process.env.NEXT_PUBLIC_BASE_URL}/api/file/${fileKey}`
+            );
+            const response = await axiosInstance.get(`/api/file/${fileKey}`, {
+                responseType: "blob",
+            });
+            const url = window.URL.createObjectURL(response.data);
+            const link = document.createElement("a");
+            link.href = url;
+            link.setAttribute(
+                "download",
+                `whizfile-transfer-${transferId}.zip`
+            );
+            document.body.appendChild(link);
+            link.click();
 
-  return (
-    <div>
-      Transfer View
-    </div>
-  );
+            if (link.parentNode) {
+                link.parentNode.removeChild(link);
+            }
+        } catch (error) {
+            console.error("Error downloading the file", error);
+        }
+    };
+
+    if (isLoading)
+        return (
+            <div className="w-full h-full flex flex-col items-center justify-center">
+                <PulseLoader color="#4539cd" size={20} speedMultiplier={0.5} />
+            </div>
+        );
+    if (error) return <div>An error has occurred: {error.message}</div>;
+
+    const formattedTime = new Date(transfer?.createdAt || 0);
+
+    return (
+        <div className="flex flex-col h-full w-full gap-2">
+            <div className="w-full h-full flex flex-col">
+                <p className="text-primary text-xl font-semibold">
+                    {transfer?.title}
+                </p>
+                <p className="text-primary text-xs">
+                    {formattedTime.toDateString()}
+                </p>
+                <p className="h-full text-primary">{transfer?.message}</p>
+            </div>
+            <button
+                onClick={() => downloadTransfer(transfer)}
+                className="h-fit w-full bg-primary rounded-xl p-2 text-secondary italic font-extrabold text-xl"
+            >
+                download
+            </button>
+        </div>
+    );
 }
 
 export default function ReceiveTransferId(context: {
-  params: { transferId: string };
+    params: { transferId: string };
 }) {
-  return (
-    <main className="w-full h-full flex flex-row items-center justify-center">
-      <Card className="w-3/5 h-3/4 flex flex-col">
-        <CardHeader className="h-fit w-full">
-          <CardTitle as="h1" className="text-primary text-4xl text-center">
-            receive a transfer
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <TransferView transferId={context.params.transferId} />
-        </CardContent>
-      </Card>
-    </main>
-  );
+    return (
+        <main className="w-full h-full flex flex-row items-center justify-center">
+            <Card className="w-3/5 h-3/4 flex flex-col">
+                <CardHeader className="h-fit w-full">
+                    <CardTitle
+                        as="h1"
+                        className="text-primary text-4xl text-center"
+                    >
+                        receive a transfer
+                    </CardTitle>
+                </CardHeader>
+                <CardContent className="h-full w-full flex flex-col space-y-2">
+                    <TransferView transferId={context.params.transferId} />
+                </CardContent>
+            </Card>
+        </main>
+    );
 }
