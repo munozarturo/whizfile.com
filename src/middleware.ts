2902e421617166a55b67b83e6b7fc16d1c31a1f7
Arturo Muñoz <101126374+munozarturo@users.noreply.github.com>
2023-11-29 10:15:15 -0500

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,14 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { RequestSchema } from "@/db/mongo";
 
+/*
+ * Middleware must only use code that can run in the edge.
+ * - Must use API routes to log requests.
+ * - Must use `fetch()` API to make requests to these endpoints because `axios` is not supported in edge.
+ * - Matcher for middleware only applies to `/api/`
+ * - Matcher for middleware makes sure to exclude `/api/requests` to avoid an infinite request logging.
+ */
+
 export async function middleware(req: NextRequest) {
     let source = req.ip ?? req.headers.get("x-real-ip");
     const forwardedFor = req.headers.get("x-forwarded-for");
@@ -33,12 +41,8 @@ export async function middleware(req: NextRequest) {
 export const config = {
     matcher: [
         /*
-         * Match all request paths except for the ones starting with:
-         * - api/requests (API route)
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
+         * Match all request paths starting with /api/ except for /api/requests
          */
-        "/((?!api/requests|_next/static|_next/image|favicon.ico).*)",
+        "/api/((?!requests$).*)",
     ],
 };
