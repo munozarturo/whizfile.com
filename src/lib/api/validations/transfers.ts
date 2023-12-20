2c7f1569283e59d06f1a38514f83bf36811afb43
Arturo Munoz <munoz.arturoroman@gmail.com>
2023-12-20 11:19:06 -0500

    [api] added request schema for transfers

--- src/lib/api/validations/transfers.ts
+++ src/lib/api/validations/transfers.ts
@@ -1,8 +1,16 @@
 import * as zod from "zod";
+import { SHA256Hash } from "./shared";
+
+const ObjectData = zod.object({
+    contents: zod.array(zod.string()),
+    size: zod.number(),
+    fileHash: SHA256Hash,
+});
 
 const TransfersReq = zod.object({
     title: zod.string(),
     message: zod.string(),
+    objectData: ObjectData,
 });
 
 export { TransfersReq };
