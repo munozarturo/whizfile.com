85089147df8590b88b1e318b561570faf93b7195
Arturo Munoz <munoz.arturoroman@gmail.com>
2023-12-20 12:40:57 -0600

    [api] added s3 presigned url generation for client side transfer uploads

--- src/app/api/transfers/route.ts
+++ src/app/api/transfers/route.ts
@@ -5,12 +5,26 @@ import { Collection, Collections, connectToDatabase } from "@/lib/db/mongo";
 import { TransfersReq } from "@/lib/api/validations/transfers";
 import { TransferIdSchema, TransferSchema } from "@/lib/db/schema/transfers";
 import { createHash, randomBytes } from "crypto";
+import { S3Client, S3ClientConfig, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 if (!process.env.UNIVERSAL_SALT) {
     throw new Error("`UNIVERSAL_SALT` environment variable is not defined.");
 }
 
 const UNIVERSAL_SALT = process.env.UNIVERSAL_SALT;
+
+if (!process.env.AWS_BUCKET) {
+    throw new Error("`AWS_BUCKET` environment variable is not defined.");
+}
+
+const AWS_BUCKET = process.env.AWS_BUCKET;
+
+if (!process.env.AWS_REGION) {
+    throw new Error("`AWS_REGION` environment variable is not defined.");
+}
+
+const AWS_REGION = process.env.AWS_REGION;
 
 function generateTransferId(): { transferId: string; hash: string } {
     var transferId = "";
@@ -45,7 +59,10 @@ export async function POST(req: NextRequest) {
         const transfers: Collection<zod.infer<typeof TransferSchema>> =
             collections.transfers;
 
-        const generateUniqueTransferId = async (): Promise<string> => {
+        const generateUniqueTransferIdHashPair = async (): Promise<{
+            transferId: string;
+            hash: string;
+        }> => {
             var transferId = generateTransferId();
 
             while (
@@ -56,21 +73,21 @@ export async function POST(req: NextRequest) {
                 transferId = generateTransferId();
             }
 
-            await transferIds.insertOne({
-                transferIdHash: transferId.hash,
-            });
-
-            return transferId.transferId;
+            return transferId;
         };
 
-        const transferId = await generateUniqueTransferId();
+        const transferIdHashPair = await generateUniqueTransferIdHashPair();
 
-        const hash = createHash("sha256");
-        hash.update(transferId);
+        let hash = createHash("sha256");
+        hash.update(transferIdHashPair.transferId);
         hash.update(UNIVERSAL_SALT);
         const transferUId = hash.digest("hex");
 
         const objectIdSalt = generateRandomSalt();
+        hash = createHash("sha256");
+        hash.update(transferUId);
+        hash.update(objectIdSalt);
+        const objectId = hash.digest("hex");
 
         const document: zod.infer<typeof TransferSchema> = {
             transferUId: transferUId,
@@ -84,13 +101,34 @@ export async function POST(req: NextRequest) {
             objectIdSalt: objectIdSalt,
         };
 
-        const uploadUrl = "not generated";
+        const s3Config: S3ClientConfig = {
+            region: AWS_REGION,
+        };
+        const s3Client: S3Client = new S3Client(s3Config);
+
+        const command: PutObjectCommand = new PutObjectCommand({
+            Bucket: AWS_BUCKET,
+            Key: objectId,
+        });
+
+        const presignedUploadUrl: string = await getSignedUrl(
+            s3Client,
+            command,
+            {
+                expiresIn: 3600,
+            }
+        );
+
+        // await transfers.insertOne(document);
+        // await transferIds.insertOne({
+        //     transferIdHash: transferIdHashPair.hash,
+        // });
 
         return NextResponse.json(
             handleResponse("Response message.", {
-                debug: document,
-                transferId: transferId,
-                uploadUrl: uploadUrl,
+                debug: document && { objId: objectId },
+                transferId: transferIdHashPair.transferId,
+                uploadUrl: presignedUploadUrl,
             }),
             { status: 200 }
         );
