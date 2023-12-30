e22fd140495102e4a3a3b6647d43ea0d08a1e99f
Arturo Munoz <munoz.arturoroman@gmail.com>
2023-12-30 16:32:26 -0600

    [ui] refactored error and loading states

--- src/app/receive/page.tsx
+++ src/app/receive/page.tsx
@@ -34,16 +34,13 @@ export default function Receive() {
     const [tryAgain, setTryAgain] = useState<boolean>(false);
     const [transferId, setTransferId] = useState<string>("");
 
-    const fetchTransfer = async (transferId: string) => {
-        const res = await axiosInstance.get(`/api/transfer/${transferId}`);
-        return res.data;
-    };
-
     const { data, error, refetch, isError, isLoading } = useQuery({
         queryKey: ["transfer", transferId],
         queryFn: async () => {
             const { urlPrefix, urlTransferId } = parseTransferUrl(transferId);
-            return fetchTransfer(urlTransferId);
+            const res = await axiosInstance.get(`/api/transfer/${transferId}`);
+
+            return res.data;
         },
         enabled: false,
         retry: false,
@@ -55,7 +52,6 @@ export default function Receive() {
         setTryAgain(false);
 
         const { data: newData } = await refetch();
-
         const { urlPrefix, urlTransferId } = parseTransferUrl(transferId);
 
         if (newData) {
@@ -90,19 +86,19 @@ export default function Receive() {
 
     const { urlPrefix, urlTransferId } = parseTransferUrl(transferId);
 
-    return (
-        <main className="w-full h-full flex flex-row justify-center items-center">
-            <Card className="w-3/5 h-fit flex flex-col">
-                <CardHeader className="h-fit w-full">
-                    <CardTitle
-                        as="h1"
-                        className="text-primary text-4xl text-center"
-                    >
-                        find a transfer
-                    </CardTitle>
-                </CardHeader>
-                <CardContent className="flex flex-col w-full h-full items-center justify-center ">
-                    {isLoading ? (
+    if (isLoading) {
+        return (
+            <main className="w-full h-full flex flex-row justify-center items-center">
+                <Card className="w-3/5 h-fit flex flex-col">
+                    <CardHeader className="h-fit w-full">
+                        <CardTitle
+                            as="h1"
+                            className="text-primary text-4xl text-center"
+                        >
+                            find a transfer
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex flex-col w-full h-full items-center justify-center ">
                         <div className="w-full h-full flex flex-col items-center justify-center">
                             <PulseLoader
                                 color="#4539cd"
@@ -110,67 +106,94 @@ export default function Receive() {
                                 speedMultiplier={0.5}
                             />
                         </div>
-                    ) : (
+                    </CardContent>
+                </Card>
+            </main>
+        );
+    } else if (isError && !tryAgain) {
+        return (
+            <main className="w-full h-full flex flex-row justify-center items-center">
+                <Card className="w-3/5 h-fit flex flex-col">
+                    <CardHeader className="h-fit w-full">
+                        <CardTitle
+                            as="h1"
+                            className="text-primary text-4xl text-center"
+                        >
+                            find a transfer
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex flex-col w-full h-full items-center justify-center ">
+                        <div className="w-full h-full flex flex-col items-center justify-center space-y-2">
+                            <h2>
+                                uh oh! it looks like a transfer with the id{" "}
+                                <span className="text-xl text-primary font-bold">
+                                    {transferId}
+                                </span>{" "}
+                                does not exist...
+                            </h2>
+                            <button
+                                onClick={() => {
+                                    setTryAgain(true);
+                                }}
+                                className="h-fit w-fit bg-primary rounded-xl p-2 text-secondary italic font-extrabold text-xl"
+                            >
+                                try a new one
+                            </button>
+                        </div>
+                    </CardContent>
+                </Card>
+            </main>
+        );
+    } else {
+        return (
+            <main className="w-full h-full flex flex-row justify-center items-center">
+                <Card className="w-3/5 h-fit flex flex-col">
+                    <CardHeader className="h-fit w-full">
+                        <CardTitle
+                            as="h1"
+                            className="text-primary text-4xl text-center"
+                        >
+                            find a transfer
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex flex-col w-full h-full items-center justify-center ">
                         <form
                             onSubmit={handleSubmit}
                             className="flex flex-col w-full h-full gap-4"
                         >
-                            {isError && !tryAgain ? (
-                                <div className="w-full h-full flex flex-col items-center justify-center space-y-2">
-                                    <h2>
-                                        uh oh! it looks like a transfer with the
-                                        id{" "}
-                                        <span className="text-xl text-primary font-bold">
-                                            {transferId}
-                                        </span>{" "}
-                                        does not exist...
-                                    </h2>
-                                    <button
-                                        onClick={() => {
-                                            setTryAgain(true);
-                                        }}
-                                        className="h-fit w-fit bg-primary rounded-xl p-2 text-secondary italic font-extrabold text-xl"
-                                    >
-                                        try a new one
-                                    </button>
+                            <div className="w-full h-fit flex flex-row p-2 border-2 border-primary rounded-md relative">
+                                <div className="absolute flex flex-row pointer-events-none w-full h-fit">
+                                    <span className="pointer-events-none w-fit text-3xl font-bold italic text-gray-500 focus:outline-none">
+                                        {transferId === ""
+                                            ? `${BASE_URL}/receive/`
+                                            : urlPrefix}
+                                    </span>
+                                    <span className="pointer-events-none w-fit text-3xl font-bold italic text-primary focus:outline-none">
+                                        {transferId === ""
+                                            ? "xxxxxx"
+                                            : urlTransferId}
+                                    </span>
                                 </div>
-                            ) : (
-                                <>
-                                    <div className="w-full h-fit flex flex-row p-2 border-2 border-primary rounded-md relative">
-                                        <div className="absolute flex flex-row pointer-events-none w-full h-fit">
-                                            <span className="pointer-events-none w-fit text-3xl font-bold italic text-gray-500 focus:outline-none">
-                                                {transferId === ""
-                                                    ? `${BASE_URL}/receive/`
-                                                    : urlPrefix}
-                                            </span>
-                                            <span className="pointer-events-none w-fit text-3xl font-bold italic text-primary focus:outline-none">
-                                                {transferId === ""
-                                                    ? "xxxxxx"
-                                                    : urlTransferId}
-                                            </span>
-                                        </div>
-                                        <input
-                                            type="text"
-                                            value={transferId}
-                                            onChange={(e) =>
-                                                setTransferId(e.target.value)
-                                            }
-                                            spellCheck="false"
-                                            className="w-full text-3xl font-bold italic text-transparent focus:outline-none "
-                                        />
-                                    </div>
+                                <input
+                                    type="text"
+                                    value={transferId}
+                                    onChange={(e) =>
+                                        setTransferId(e.target.value)
+                                    }
+                                    spellCheck="false"
+                                    className="w-full text-3xl font-bold italic text-transparent focus:outline-none "
+                                />
+                            </div>
 
-                                    <input
-                                        type="submit"
-                                        value="search"
-                                        className="cursor-pointer h-fit w-full bg-primary rounded-xl p-2 text-secondary italic font-extrabold text-2xl"
-                                    />
-                                </>
-                            )}
+                            <input
+                                type="submit"
+                                value="search"
+                                className="cursor-pointer h-fit w-full bg-primary rounded-xl p-2 text-secondary italic font-extrabold text-2xl"
+                            />
                         </form>
-                    )}
-                </CardContent>
-            </Card>
-        </main>
-    );
+                    </CardContent>
+                </Card>
+            </main>
+        );
+    }
 }
